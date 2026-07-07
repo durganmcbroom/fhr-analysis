c3df4cda2cfee633515a2ccb7e7c5d15b2e698f0
durganmcbroom <durganmcbroom@gmail.com>
2026-07-07 12:57:19 -0400

    generate_training_snippets.sh update.

--- lib/tune-ssnet/generate_training_snippets.sh
+++ lib/tune-ssnet/generate_training_snippets.sh
@@ -1,3 +1,4 @@
 BASEDIR=$(dirname $0)
 echo $BASEDIR
-python3 "$BASEDIR/../../src/bin/generate_training_snippets.py" training_clips.yaml --out-dir="training/training_clips_mono_v6/"
+
+python3 "$BASEDIR/../../src/bin/generate_training_snippets.py" "$BASEDIR/training_clips.yaml" --out-dir="$BASEDIR/training/training_clips_mono_v6/"
