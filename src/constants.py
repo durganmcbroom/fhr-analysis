398d740fcc9405daa90fdac5b41554dc5fbb9b43
durganmcbroom <durganmcbroom@gmail.com>
2026-07-22 13:22:08 -0400

    RFP (rough, fine pass) training for FUNet. Going to see if this makes an improvement, and if so, train on SSNet as well.

--- src/constants.py
+++ src/constants.py
@@ -8,7 +8,7 @@ DEFAULT_DATA_DIR = PROJECT_DIR + BANNER_TEST_DIR
 
 # Fine-tuned SSNet checkpoints (lib/tune-ssnet)
 # FETAL_MODEL = "tuned-model-v7 (SASDR)"
-FETAL_MODEL = "tuned-model-v10"
+FETAL_MODEL = "tuned-model-v13"
 FETAL_MODEL_PATH = os.path.join(PROJECT_DIR, f"lib/tune-ssnet/models/{FETAL_MODEL}/model_best.pt")
 FETAL_MODEL_CFG = os.path.join(PROJECT_DIR, f"lib/tune-ssnet/models/{FETAL_MODEL}/model.yaml")
 MATERNAL_MODEL_PATH = os.path.join(PROJECT_DIR, "lib/tune-ssnet/models/maternal-tuned-model-v2/model_best.pt")
@@ -20,7 +20,7 @@ NEOSSNET_MODEL_CFG = os.path.join(PROJECT_DIR, "lib/neossnet/models/model.yaml")
 
 # FUNet beat-activity model (lib/funet)
 # FUNET_CONFIG = os.path.join(PROJECT_DIR, "lib/funet/fetal-config.yaml")
-FUNET_MODEL = "funet-v24"
+FUNET_MODEL = "funet-v26"
 FUNET_CONFIG = os.path.join(PROJECT_DIR, f"lib/funet/models/{FUNET_MODEL}/fetal-config.yaml")
 FUNET_MODEL_PATH = os.path.join(PROJECT_DIR, f"lib/funet/models/{FUNET_MODEL}/model_best.pt")
 
