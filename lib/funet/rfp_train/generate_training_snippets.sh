84e6e99fc99c47bedf5c33c180191c7c98e0df9e
durganmcbroom <durganmcbroom@gmail.com>
2026-07-22 13:34:27 -0400

    Update

--- lib/funet/rfp_train/generate_training_snippets.sh
+++ lib/funet/rfp_train/generate_training_snippets.sh
@@ -1,6 +1,6 @@
 BASEDIR=$(dirname $0)
 
-source "$BASEDIR/../../.venv/bin/activate"
+source "$BASEDIR/../../../.venv/bin/activate"
 
 python3 "$BASEDIR/../../../src/bin/generate_training_snippets.py" "$BASEDIR/rough_pass_training_clips.yaml" --out-dir="$BASEDIR/training/rough_v1/"
 python3 "$BASEDIR/../../../src/bin/generate_training_snippets.py" "$BASEDIR/fine_pass_training_clips.yaml" --out-dir="$BASEDIR/training/fine_v1/"
