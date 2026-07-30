ae4d4c29d5b93f060bfe940e16b4a85840adb412
durganmcbroom <durganmcbroom@gmail.com>
2026-07-30 16:37:15 -0400

    V2 for TSLNet

--- jobs/train_tslnet.sh
+++ jobs/train_tslnet.sh
@@ -14,7 +14,7 @@ chmod a+x setup.sh
 ./setup.sh
 
 chmod a+x lib/tslnet/generate_training_snippets.sh
-lib/tslnet/generate_training_snippets.sh
+#lib/tslnet/generate_training_snippets.sh
 
 # The first run pulls the TimesFM checkpoint (~2 GB) into the Hugging Face cache. Point
 # HF_HOME at shared storage if the compute node's home is small or not persisted.
