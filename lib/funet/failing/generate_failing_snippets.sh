216b4942ea5a73b791c30b5fdd0c20ace4b7a32f
durganmcbroom <durganmcbroom@gmail.com>
2026-08-13 15:29:43 -0700

    Test and train suites for both

--- lib/funet/failing/generate_failing_snippets.sh
+++ lib/funet/failing/generate_failing_snippets.sh
@@ -1,4 +1,5 @@
 BASEDIR=$(cd "$(dirname "$0")" && pwd)
 ROOT=$(cd "$BASEDIR/../../.." && pwd)
 
-poetry -P "$ROOT" run fhr-mine-failures "$BASEDIR/../models/funet-v36/config.yaml" --model-dir="$BASEDIR/../models/funet-v36"  --snippet-dir="$BASEDIR/../training/stereo_v13/" --out-dir="$BASEDIR/../training/stereo_v14(failed)/" --metric hr_agree --threshold 0.7
+poetry -P "$ROOT" run fhr-mine-failures "$BASEDIR/../models/funet-v36/config.yaml" --model-dir="$BASEDIR/../models/funet-v36"  --snippet-dir="$BASEDIR/../training/stereo_v13/fetal-train" --out-dir="$BASEDIR/../training/stereo_v14(failed)/fetal-train" --metric hr_agree --threshold 0.7
+poetry -P "$ROOT" run fhr-mine-failures "$BASEDIR/../models/funet-v36/config.yaml" --model-dir="$BASEDIR/../models/funet-v36"  --snippet-dir="$BASEDIR/../training/stereo_v13/fetal-test" --out-dir="$BASEDIR/../training/stereo_v14(failed)/fetal-test" --metric hr_agree --threshold 0.7
